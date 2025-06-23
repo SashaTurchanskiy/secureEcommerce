7d8cb69bba874206266a92a035d918b451db3038
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-06-23 10:56:26 +0300

    set up project

---
+++ src/main/java/com/secureecomerce/secureEcommerce/SecureEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.secureecomerce.secureEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureecomerce/secureEcommerce/SecureEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureecomerce.secureEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
